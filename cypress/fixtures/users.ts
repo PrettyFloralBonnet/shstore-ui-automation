9078d2bf092094f4b00c69ee5d146691be532319
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2025-02-16 18:25:17 +0100

    create fixture with user data

---
+++ cypress/fixtures/users.ts
@@ -0,0 +1,27 @@
+export interface User {
+    firstName: string;
+    lastName: string;
+    address: {
+        street: string;
+        flatNumber: string;
+        postalCode: string;
+        city: string;
+    };
+    phoneNumber: string;
+    email: string;
+    password: string;
+}
+
+export const TEST_USER: User = {
+    firstName: 'Tester',
+    lastName: 'McTest',
+    address: {
+        street: 'Aleje Jerozolimskie',
+        flatNumber: '1a',
+        postalCode: '00-495',
+        city: 'Warsaw',
+    },
+    phoneNumber: '+48 001 010 100',
+    email: 'shstoretest1@gmail.com',
+    password: '',  // substitute to run tests locally
+}
