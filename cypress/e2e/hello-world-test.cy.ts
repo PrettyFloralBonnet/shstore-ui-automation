9cbf18116b82e6db06c05a031ec274ba9af4a708
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2025-02-17 02:08:58 +0100

    test(e2e): verify cart contents

--- cypress/e2e/hello-world-test.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Hello world', () => {
-    it('should load the main store page', () => {
-        cy.visit('/');
-        cy.url().should('contain', 'shstore.eu');
-        cy.get('#logo').should('be.visible');
-    });
-});
